d37f708685d2c562f2d62ffed9721629848a2da8
AaronVelasco93 <aaronvelasco1993@outlook.com>
2024-07-16 23:29:14 -0600

    Tecnologias re revisar

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # aws
 serverless con aws
+
+https://graphql.org/
+Atlas en mongo
+https://cloud.mongodb.com/v2/669755bd6496143cc841a505#/overview
---
+++ node/1/4.fs-async,await-parallel.mjs
@@ -0,0 +1,9 @@
+import { readFile } from "fs/promises";
+
+Promise.all([
+    readFile('./archivo.txt','utf-8'),
+    readFile('./archivo2.txt','utf-8')
+]).then(([text,secondText])=>{
+    console.log('Primer archivo',text)
+    console.log('Segundo archivos',secondText)
+})
