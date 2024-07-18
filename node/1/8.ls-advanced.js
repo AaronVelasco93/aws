335ca3b59f68bbf5e3e7a9bfe7f65b6d80fea3f2
AaronVelasco93 <aaronvelasco1993@outlook.com>
2024-07-17 23:39:49 -0600

    uso de archivos final

--- node/1/8.ls-advanced.js
+++ node/1/8.ls-advanced.js
@@ -1,20 +1,41 @@
   const fs = require('node:fs/promises');
+  const path = require('node:path');
   const folder = process.argv[2]?? '.';
   
+  async function ls (folder){
+    let files;
+    try{
+        files = await fs.readdir(folder)
   
-  fs.readdir(folder)
-  .then(files =>{
-      files.forEach(file =>{
-          console.log(file)
-      })
-  })
-  .catch(err =>{
-      if(err){
-          console.error('Error leyendo directorio: ',err)
-          return;
-      }
-  })
+    }catch{
+        console.error(`No se pudo leer el directorio ${folder}`);
+        process.exit(1);
+    }
+
+    const filesPromises = files.map( async file =>{
+         const filePath=path.join(folder, file)
+         let stats
+         try{
+             stats = await fs.stat(filePath) //informacion del archivos
+         }catch{
+            console.error(`No se pudo leer el archivo ${filePath}`);
+            process.exit(1);
+         }
+         const isDirectory = stats.isDirectory()
+         const fileType = isDirectory ? 'd': 'f ';
+         const fileSize = stats.size;
+         const fileModified = stats.mtime.toLocaleString() ;
+         return `${fileType} ${file.padEnd(20)} ${fileSize.toString().padStart(10)} ${fileModified}`
+    })
+
+   const filesInfo = await Promise.all(filesPromises)
+    
+    filesInfo.forEach(fileInfo => console.log(fileInfo))
+   
+  }
   
+ls(folder)
+
 /*
 const fs = require('fs').promises;
 const folder = process.argv[2] ?? '.';
@@ -28,4 +49,21 @@ fs.readdir(folder)
   .catch(err => {
     console.error('Error leyendo directorio:', err);
   });
+  */
+
+
+  /*
+  
+   .then(files =>{
+        files.forEach(file =>{
+            const filePath = path.join(folder, file)
+            fs.stat(filePath)
+        })
+    })
+    .catch(err =>{
+        if(err){
+            console.error('Error leyendo directorio: ',err)
+            return;
+        }
+    })
   */
